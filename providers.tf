2cf6c85d154a95d95e80bbe81c764bd33d527d07
Apurva_Ashutosh <apooorva01@gmail.com>
2022-11-21 18:22:59 +0530

    Updted ec2.tf & providers.tf

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
+    }
+  }
+}
+provider "aws" {
+  profile                 = "user1"
+  region                  = "ap-south-1"
+  shared_credentials_file = "C:/Users/user/.aws/credentials"
+}
